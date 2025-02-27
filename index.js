e50b0358ce0139deffcb4e41c43ff952f456d1f9
Anuj171018 <anujshrivastava2022@gmail.com>
2025-02-27 17:05:13 +0530

    Success

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+
+
+
 function sign() {
     let fname = document.querySelector('#fname').value;
     let lname = document.querySelector('#lname').value;
@@ -8,20 +11,20 @@ function sign() {
     let password = document.querySelector('#pass').value;
 
     let abc = {
-        gmail :document.querySelector("#email").value,
-        passw :document.querySelector("#pass").value
-    }
-    localStorage.setItem("userdata",JSON.stringify(abc))
+        gmail: email,
+        passw: password
+    };
 
+    localStorage.setItem("userdata", JSON.stringify(abc));
+    console.log("User registered:", abc); // Debugging
 
-
+    // Validation checks
     if (fname === '') {
         alert("Please enter your First Name");
         document.querySelector('#fname').focus();
         return false;
     }
 
-    
     if (lname === '') {
         alert("Please enter your Last Name");
         document.querySelector('#lname').focus();
@@ -63,23 +66,116 @@ function sign() {
 }
 
 
-let data=JSON.parse(localStorage.getItem("userdata"))
-const up =()=>{
-    let obj ={
-        smail :document.querySelector("#mail").value,
-        pword :document.querySelector("#pword").value,
-    }
-    
-    if(data.gmail !=obj.smail)
-    {
-        alert("Please enter correct Email")
-        return false
-    }
-    else if (data.passw!=obj.pword){
-        alert("Please enter correct Password")
-        return false
+const up = () => {
+    let storedData = localStorage.getItem("userdata");
+
+    if (!storedData) {
+        alert("No user data found. Please sign up first.");
+        return false;
     }
 
+    let data = JSON.parse(storedData);
+    console.log("Stored data:", data); 
 
-    
+    let enteredEmail = document.querySelector("#mail").value;
+    let enteredPassword = document.querySelector("#pword").value;
+
+    if (enteredEmail !== data.gmail) {
+        alert("Incorrect Email!");
+        return false;
+    }
+
+    if (enteredPassword !== data.passw) {
+        alert("Incorrect Password!");
+        return false;
+    }
+
+    alert("Login Successful!");
+    return true; 
+};
+
+
+let fair 
+// let a="M.P Nagar"
+// let b="Ashoka Garden"
+
+// function dist(){
+//  let pick=document.querySelector("#pickup").value
+//  let destination = document.querySelector("#destination").value
+
+//  if(pick=""){
+//     alert("Please enter the pickup")
+//     return false
+//  }
+//  if(destination=""){
+//     alert("Please enter the destination")
+//     return false
+//  }
+//  if(pick==a && destination==b){
+//     document.querySelector("#head").innerHTML="45"
+//     // return true;
+//  }
+
+//  return true;
+
+// }
+
+
+// let a = "M.P Nagar";
+// let b = "Ashoka Garden";
+
+// function dist() {
+//     let pick = document.querySelector("#pickup").value
+//     let destination = document.querySelector("#destination").value
+
+//      if(pick==""){
+//     alert("Please enter the pickup")
+//     return false
+//  }
+//  if(destination==""){
+//     alert("Please enter the destination")
+//     return false
+//  }
+
+//     if (pick === a && destination === b) {
+//         let headElement = document.querySelector("#head");
+
+//         if (headElement) {
+//             headElement.innerHTML = "Distance: 45 km"; 
+//             console.log("45");
+            
+//         } else {
+//             alert("Distance: 45 km"); 
+//         }
+
+//         return false; 
+//     }
+
+//     return true; 
+// }
+
+let a = "M.P Nagar";
+let b = "Ashoka Garden";
+
+function dist() {
+    let pick = document.querySelector("#pickup").value
+    let destination = document.querySelector("#destination").value
+
+    if (pick === "") {
+        alert("Please enter the pickup location");
+        return false;
+    }
+
+    if (destination === "") {
+        alert("Please enter the destination");
+        return false;
+    }
+
+    if (pick === a && destination === b) {
+        localStorage.setItem("distance", "Distance: 45 km"); 
+        return true; 
+    } else {
+        alert("Invalid pickup or destination");
+        return false; 
+    }
 }
